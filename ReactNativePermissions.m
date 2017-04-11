226c95e1f4c3dfbffa90e9a64f83c9fa4f98d6db
Martin Koo <koo.martin@gmail.com>
2017-04-11 10:27:17 -0400

    removing unneeded speech and bluetooth permissions

--- ReactNativePermissions.m
+++ ReactNativePermissions.m
@@ -21,19 +21,16 @@
 #endif
 
 #import "RNPLocation.h"
-#import "RNPBluetooth.h"
 #import "RNPNotification.h"
 #import "RNPAudioVideo.h"
 #import "RNPEvent.h"
 #import "RNPPhoto.h"
 #import "RNPContacts.h"
 #import "RNPBackgroundRefresh.h"
-#import "RNPSpeechRecognition.h"
 
 @interface ReactNativePermissions()
 @property (strong, nonatomic) RNPLocation *locationMgr;
 @property (strong, nonatomic) RNPNotification *notificationMgr;
-@property (strong, nonatomic) RNPBluetooth *bluetoothMgr;
 @end
 
 @implementation ReactNativePermissions
@@ -102,17 +99,11 @@ RCT_REMAP_METHOD(getPermissionStatus, getPermissionStatus:(RNPType)type json:(id
         case RNPTypeReminder:
             status = [RNPEvent getStatus:@"reminder"];
             break;
-        case RNPTypeBluetooth:
-            status = [RNPBluetooth getStatus];
-            break;
         case RNPTypeNotification:
             status = [RNPNotification getStatus];
             break;
         case RNPTypeBackgroundRefresh:
             status = [RNPBackgroundRefresh getStatus];
-            break;
-        case RNPTypeSpeechRecognition:
-            status = [RNPSpeechRecognition getStatus];
             break;
         default:
             break;
@@ -140,13 +131,8 @@ RCT_REMAP_METHOD(requestPermission, permissionType:(RNPType)type json:(id)json r
             return [RNPEvent request:@"event" completionHandler:resolve];
         case RNPTypeReminder:
             return [RNPEvent request:@"reminder" completionHandler:resolve];
-        case RNPTypeBluetooth:
-            return [self requestBluetooth:resolve];
         case RNPTypeNotification:
             return [self requestNotification:json resolve:resolve];
-        case RNPTypeSpeechRecognition:
-            return [RNPSpeechRecognition request:resolve];
-        default:
             break;
     }
     
@@ -186,18 +172,5 @@ RCT_REMAP_METHOD(requestPermission, permissionType:(RNPType)type json:(id)json r
     [self.notificationMgr request:types completionHandler:resolve];
 
 }
-
-
-- (void) requestBluetooth:(RCTPromiseResolveBlock)resolve
-{
-    if (self.bluetoothMgr == nil) {
-        self.bluetoothMgr = [[RNPBluetooth alloc] init];
-    }
-    
-    [self.bluetoothMgr request:resolve];
-}
-
-
-
 
 @end
